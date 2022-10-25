f197af20139a2734b3354361113e71453ecee454
faazizpro <fahadafs11@gmail.com>
2022-10-25 20:51:51 +0600

    Set API to get it on server 5000

--- index.js
+++ index.js
@@ -2,6 +2,13 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 
+
+const categories = require('./data/course.json');
+
+app.get('/course', (req, res) => {
+    res.send(categories)
+})
+
 app.get('/', (req, res) => {
   res.send('Course API is Running')
 })
