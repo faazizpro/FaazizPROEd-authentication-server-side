993ce4938b42a867aa76ad334863febc1cf05e20
faazizpro <fahadafs11@gmail.com>
2022-10-26 16:59:45 +0600

    Created enroll api

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
+const cors = require('cors')
 
+app.use(cors())
 
 const categories = require('./data/course.json');
 
@@ -12,6 +14,19 @@ app.get('/course', (req, res) => {
 app.get('/', (req, res) => {
   res.send('Course API is Running')
 })
+
+app.get('/course/:id', (req,res) => {
+  const id = req.params.id;
+  const singleCourse = categories.find(sc => sc.id == id)
+  res.send(singleCourse)
+})
+
+app.get('/enroll/:id', (req,res) => {
+  const id = req.params.id;
+  const enrollCourse = categories.find(sc => sc.id == id)
+  res.send(enrollCourse)
+})
+
 
 app.listen(port, () => {
   console.log('FaazizPROEd Course Site is running on port', port);
